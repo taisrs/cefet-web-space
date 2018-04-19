702f6bd9322d2cff76f034129e2ec9d997bb5a65
Taís Rocha Silva <silva.taisrocha@gmail.com>
2018-04-19 20:43:31 -0300

    Concludes the exercise

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,19 @@
-// Faça o exercício dos parágrafos aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+// recupera do DOM os botões existentes no article de id #aliens
+let buttonList = document.querySelectorAll('#aliens .botao-expandir-retrair');
+
+// percorre a lista de botões e atrela uma função ao evento de 'click' para cada um deles
+for (let buttonEl of buttonList) {
+    buttonEl.addEventListener('click', function (e) {
+        expand_retract(buttonEl);
+    });
+}
+
+function expand_retract(buttonEl) {
+    if (buttonEl.innerText === '+') {
+        buttonEl.parentNode.classList.add('expandido');
+        buttonEl.innerText = '-';
+    } else {
+        buttonEl.innerText = '+';
+        buttonEl.parentNode.classList.remove('expandido');
+    }
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,6 +1,3 @@
-// Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
-
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
   todasAsImagens = [
     'philae-parts.jpg',
@@ -9,3 +6,57 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+  let gallery = document.querySelector('#slide');
+  let prevEl = document.querySelector('#anterior');
+  let nextEl = document.querySelector('#proximo');
+  let current = document.querySelector('#slide img');
+
+  for (let imageEl of todasAsImagens) {
+      let source = servidorDasImagens+imageEl;
+      if (current.src !== source) {
+          let dataEl = document.createElement('img');
+          dataEl.src = source;
+          dataEl.classList.add('oculto');
+          gallery.appendChild(dataEl);
+      }
+  }
+
+  prevEl.addEventListener('click', leftSlide);
+  nextEl.addEventListener('click', rightSlide);
+
+  function leftSlide() {
+      let slides = document.querySelectorAll('#slide img');
+
+      for (let i = 0; i < slides.length; i++) {
+          if (!slides[i].classList.contains('oculto')) {
+              slides[i].classList.add('oculto');
+
+              if (i === 0) {
+                  slides[slides.length - 1].classList.remove('oculto');
+              } else {
+                  slides[i - 1].classList.remove('oculto');
+              }
+
+              return;
+          }
+      }
+  }
+
+  function rightSlide() {
+      let slides = document.querySelectorAll('#slide img');
+
+      for (let i = 0; i < slides.length; i++) {
+          if (!slides[i].classList.contains('oculto')) {
+              slides[i].classList.add('oculto');
+
+              if (i === slides.length - 1) {
+                  slides[0].classList.remove('oculto');
+              } else {
+                  slides[i + 1].classList.remove('oculto');
+              }
+
+              return;
+          }
+      }
+  }
